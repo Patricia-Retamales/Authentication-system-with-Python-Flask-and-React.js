c813159f147f6bbf3b8431330ee6c2615e7c0d3d
patricia <retamalespatricia01@gmail.com>
2022-08-25 02:22:28 +0000

    se agrego validacion de contraseña repetida

--- src/front/js/component/Signup.js
+++ src/front/js/component/Signup.js
@@ -1,19 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
+  const [errormensaje, setErrormensaje] = useState("");
   const { store, actions } = useContext(Context);
   let navigate = useNavigate();
+
   const handleSubmit = (evento) => {
     evento.preventDefault();
     console.log(evento.target[0].value);
     console.log(evento.target[1].value);
     const correo = evento.target[0].value;
     const pass = evento.target[1].value;
-    actions.registraFuncion(correo, pass);
-    navigate("../login", { replace: true });
+    const passrepetida = evento.target[2].value;
+    if (pass !== passrepetida) {
+      console.log("entro aca ");
+      setErrormensaje("las contraseñas tienen que ser iguales");
+    } else {
+      actions.registraFuncion(correo, pass);
+      navigate("../login", { replace: true });
+    }
   };
+  console.log("mensaje", errormensaje);
 
   return (
     <div>
@@ -24,6 +33,7 @@ export const Signup = () => {
           <input placeholder="Contraseña" />
           <input placeholder="Repetir Contraseña" />
           <button>Enviar</button>
+          <h4>{errormensaje}</h4>
         </form>
       </div>
     </div>
